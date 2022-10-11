7a5efc32a96e7f779ef6daba39bc7c884ce68b0e
Dedi surahmin <108617513+callmededi@users.noreply.github.com>
2022-10-11 17:15:30 +0700

    STUDI KASUS 01

---
+++ Game.java
@@ -0,0 +1,22 @@
+public class Game {
+    private Player player1;
+    private Player player2;
+
+    public Game(Player player1,Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Player player1){
+        this.player1 = player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+
+    }
+}
---
+++ Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    private int strategi;
+    private int skor;
+
+    public Player (int strategi,int skor) {
+        strategi = strategi;
+        this.skor = skor;
+    }
+}
